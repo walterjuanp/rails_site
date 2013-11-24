b4ad044f0fe30e06bd315e12f76dad8c47924450
Walter Juan <walterjuanp@gmail.com>
2013-11-24 21:42:00 +0100

    Fix slug validator

--- lib/slug_validator.rb
+++ lib/slug_validator.rb
@@ -1,6 +1,6 @@
 class SlugValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A[a-z|\-]+\z/
+    unless value =~ /\A[a-z|\d|\-]+\z/
       record.errors[attribute] << (options[:message] || "is invalid. Only downcase letters, numbers and hyphens (-) are allowed")
     end
   end

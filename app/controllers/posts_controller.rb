34855cf2e02a4a4f18901604a9b3a14cec4e2630
Walter Juan <walterjuanp@gmail.com>
2013-12-14 20:52:04 +0100

    add pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.paginate(:page => params[:page], :per_page => 10).order('published_at DESC')
+    @posts = Post.paginate(:page => params[:page], :per_page => ELEMS_PER_PAGE).order('published_at DESC')
   end
   def show
     @post = Post.find(params[:id])
